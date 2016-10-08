9cf0caa6b43baee4d587664972bdd74990f4f453
ATalaveraDev <atalavera86@gmail.com>
2016-10-08 16:05:25 +0100

    Commit 2 en master

--- wizard.js
+++ wizard.js
@@ -2,4 +2,8 @@ var wizard = (function () {
   this.init = function () {
     console.log('The destruction of the wizard');
   };
+
+  this.fireball = function () {
+    alert('Launching fireball');
+  };
 })();

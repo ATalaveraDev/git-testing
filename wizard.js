69c8bbe87ec7b23d67e96ff186ee968c0c0254db
Melanie Durá Gil <melanie@air-de-melanie.home>
2016-10-08 16:08:56 +0100

    usuario 1 commit 2

--- wizard.js
+++ wizard.js
@@ -2,4 +2,9 @@ var wizard = (function () {
   this.init = function () {
     console.log('Mel is the best');
   };
+
+  this.romer = function () {
+  	var pepe = 'Soy Pepe';
+  	alert(pepe);
+  };
 })();
